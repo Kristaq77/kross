633a64a577a1e9174ac58f94791ff9bd6f71a4b0
Kristaq <159691233+Kristaq77@users.noreply.github.com>
2026-07-16 22:43:23 +0300

    Update hello_world.c

--- examples/hello_world.c
+++ examples/hello_world.c
@@ -1,20 +1,21 @@
+// -------------------------------------
 #define KROSS_IMPLEMENTATION
 #define KROSS_STRIP_PREFIX
 #include "kross.h"
-
+// -------------------------------------
 int main(void)
 {
   Kanvas* kv = kv_init(800, 600);
   kw_init(kv, " Hello World");
   kw_target_fps(60);
-
+  // --------------
   while (!kw_should_close())
   {
     kv_start(kv);
     kv_fill(kv, KLEAN_BLACK);
-
+    // --------------
     kv_rect(kv, 400-25, 300-25, 50, 50, KLEAN_YELLOW);
-
+    // --------------
     kv_stop(kv);
   }
 
@@ -22,3 +23,4 @@ int main(void)
   kw_free();
   return 0;
 }
+// -------------------------------------
