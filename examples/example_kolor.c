2440f93b3771ca9fbce9a10eea07c54db78a6c6f
Kristaq <159691233+Kristaq77@users.noreply.github.com>
2026-07-24 15:23:53 +0300

    Update example_kolor.c

--- examples/example_kolor.c
+++ examples/example_kolor.c
@@ -1,28 +1,29 @@
+// -------------------------------------
 #define KROSS_IMPLEMENTATION
-#define KROSS_STRIP_PREFIX
 #include "kross.h"
-
+// -------------------------------------
 int main(void)
 {
   Kanvas* kv = kv_init(800, 600);
   kw_init(kv, "Title");
+  // --------------
   int n = 360;
-
+  // --------------
   while (!kw_should_close())
   {
     kv_start(kv);
     kv_fill(kv, KLEAN_BLACK);
-
+    // --------------
     for (int i = 0; i < n; ++i)
     {
       int x0 = i * kv->w / n;
       int x1 = (i + 1) * kv->w / n;
       int width = x1 - x0;
-
+      // --------------
       float hue = (float)i/(float)n;
       float sat = 0.75f;
       float val = 1.0f;
-      
+      // --------------
       Kolor kl = kl_from_hsv(hue, sat, val);
       kv_rect(kv, x0, 0, width, kv->h, kl);
     }
@@ -34,3 +35,4 @@ int main(void)
   kv_free(kv);
   return 0;
 }
+// -------------------------------------
